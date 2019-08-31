515bcbd755788fb85ba0550f6a2f76765041e2bb
rajesh3679 <rajesh.g3679@gmail.com>
2019-08-31 12:03:47 +0530

    added service type properties

--- src/main/java/com/event/business/util/ServiceTypeConfig.java
+++ src/main/java/com/event/business/util/ServiceTypeConfig.java
@@ -1,6 +1,7 @@
 package com.event.business.util;
 
 import java.util.HashMap;
+import java.util.TreeSet;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,16 +12,15 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "services")
 public class ServiceTypeConfig {
 	
-	private HashMap<String, String> offered;
+	private HashMap<String, TreeSet<String>> offered;
 
-	public HashMap<String, String> getOffered() {
+	public HashMap<String, TreeSet<String>> getOffered() {
 		return offered;
 	}
 
-	public void setOffered(HashMap<String, String> offered) {
+	public void setOffered(HashMap<String, TreeSet<String>> offered) {
 		this.offered = offered;
 	}
-	
 	
 
 }
